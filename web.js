56ad0c928e10dfd7769f3e023a2d19e1eefd5af6
xgIzne <123@fakestreet.springfield>
2013-07-10 03:56:07 +0000

    another update

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var filename = './index.html';
+    var buf = fs.readFileSync(filename);
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
